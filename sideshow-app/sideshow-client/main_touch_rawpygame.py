58bf8abfa31750764c708d41bf239bec14775d47
Gowtam Lal <glal14@gmail.com>
2020-04-21 03:16:16 -0400

    Center the text based on the icon rect.

--- sideshow-app/sideshow-client/main_touch_rawpygame.py
+++ sideshow-app/sideshow-client/main_touch_rawpygame.py
@@ -104,21 +104,21 @@ class Sideshow():
         
         lcd.fill((0,0,0))
 
-        text_surface = font_big.render('%s' % metrics['cpu'], True, Sideshow.WHITE)
-        rect = text_surface.get_rect(center=(320 - Sideshow.MARGIN - 40, 240 - Sideshow.MARGIN - 40))
-        lcd.blit(text_surface, rect)
-
         icon_rect = pygame.Rect(Sideshow.MARGIN, 240 - Sideshow.MARGIN - 100, 100, 100)
         icon_img = pygame.transform.scale(pygame.image.load('resources/cpu_line.png'), (icon_rect.width, icon_rect.height))
         lcd.blit(icon_img, icon_rect)
 
-        text_surface = font_big.render('%s' % metrics['gpu'], True, Sideshow.WHITE)
-        rect = text_surface.get_rect(center=(Sideshow.MARGIN + 45, Sideshow.MARGIN + 55))
+        text_surface = font_big.render('%s' % metrics['cpu'], True, Sideshow.WHITE)
+        rect = text_surface.get_rect(center=(icon_rect.center[0], Sideshow.MARGIN + 55))
         lcd.blit(text_surface, rect)
 
         icon_rect = pygame.Rect(320 - Sideshow.MARGIN - 100, Sideshow.MARGIN - 5, 100, 100)
         icon_img = pygame.transform.scale(pygame.image.load('resources/gpu_line.png'), (icon_rect.width, icon_rect.height))
         lcd.blit(icon_img, icon_rect)
+
+        text_surface = font_big.render('%s' % metrics['gpu'], True, Sideshow.WHITE)
+        rect = text_surface.get_rect(center=(icon_rect.center[0], 240 - Sideshow.MARGIN - 40))
+        lcd.blit(text_surface, rect)
 
         pygame.display.update()
       except Exception as e:
@@ -135,7 +135,7 @@ def init_logging():
   console_handler = logging.StreamHandler()
   console_handler.setFormatter(logging.Formatter(log_format))
   logger = logging.getLogger()
-  logger.setLevel(logging.DEBUG)
+  logger.setLevel(logging.INFO)
   logger.addHandler(console_handler)
   return logger
 
