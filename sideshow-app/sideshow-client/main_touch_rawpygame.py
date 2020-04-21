831329ce6eae0a89462039d682b0dde9274b5250
Gowtam Lal <glal14@gmail.com>
2020-04-21 14:05:52 -0400

    Don't bother with SVGs, this is a very static rendering environment.

--- sideshow-app/sideshow-client/main_touch_rawpygame.py
+++ sideshow-app/sideshow-client/main_touch_rawpygame.py
@@ -55,45 +55,6 @@ class Sideshow():
     self.running = False
     sys.exit(0)
 
-  @staticmethod
-  def invert_img(img):
-    inv = pygame.Surface(img.get_rect().size, pygame.SRCALPHA)
-    inv.fill((255,255,255,255))
-    inv.blit(img, (0,0), None, pygame.BLEND_RGB_SUB)
-    return inv
-
-  @staticmethod
-  def load_svg(filename, scale=None, size=None, clip_from=None, fit_to=None):
-    """Returns Pygame Image object from rasterized SVG
-
-    If scale (float) is provided and is not None, image will be scaled.
-    If size (w, h tuple) is provided, the image will be clipped to specified size.
-    If clip_from (x, y tuple) is provided, the image will be clipped from specified point.
-    If fit_to (w, h tuple) is provided, image will be scaled to fit in specified rect.
-    """
-    svg = Parser.parse_file(filename)
-    tx, ty = 0, 0
-    if size is None:
-        w, h = svg.width, svg.height
-    else:
-        w, h = size
-        if clip_from is not None:
-            tx, ty = clip_from
-    if fit_to is None:
-        if scale is None:
-            scale = 1
-    else:
-        fit_w, fit_h = fit_to
-        scale_w = float(fit_w) / svg.width
-        scale_h = float(fit_h) / svg.height
-        scale = min([scale_h, scale_w])
-    rast = Rasterizer()
-    req_w = int(w * scale)
-    req_h = int(h * scale)
-    buff = rast.rasterize(svg, req_w, req_h, scale, tx, ty)
-    image = pygame.image.frombuffer(buff, (req_w, req_h), 'ARGB')
-    return image
-
   def render_cpu_gpu_temps_page(self):
     LARGE_SQUARE_ICON_SIZE = 100
     icon_rect = pygame.Rect(Sideshow.MARGIN, Sideshow.SCREEN_HEIGHT - Sideshow.MARGIN - LARGE_SQUARE_ICON_SIZE, LARGE_SQUARE_ICON_SIZE, LARGE_SQUARE_ICON_SIZE)
