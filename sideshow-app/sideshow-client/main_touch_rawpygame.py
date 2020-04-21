ea1c87019cf8a37aa8325e2b14f7586d34422e6c
Gowtam Lal <glal14@gmail.com>
2020-04-21 13:28:10 -0400

    More cleanup.

--- sideshow-app/sideshow-client/main_touch_rawpygame.py
+++ sideshow-app/sideshow-client/main_touch_rawpygame.py
@@ -32,8 +32,10 @@ class MetricsRefresher:
         time.sleep(1)
 
 class Sideshow():
+  SCREEN_WIDTH = 320
+  SCREEN_HEIGHT = 240
   MARGIN = 20
-  WHITE = (255, 255, 240)
+  OFF_WHITE = (255, 255, 240)
 
   def __init__(self):
     os.putenv('SDL_FBDEV', '/dev/fb1')
@@ -90,20 +92,21 @@ class Sideshow():
     return image
 
   def render_cpu_gpu_temps_page(self):
-    icon_rect = pygame.Rect(Sideshow.MARGIN, 240 - Sideshow.MARGIN - 100, 100, 100)
+    LARGE_SQUARE_ICON_SIZE = 100
+    icon_rect = pygame.Rect(Sideshow.MARGIN, Sideshow.SCREEN_HEIGHT - Sideshow.MARGIN - LARGE_SQUARE_ICON_SIZE, LARGE_SQUARE_ICON_SIZE, LARGE_SQUARE_ICON_SIZE)
     icon_img = pygame.transform.scale(pygame.image.load('resources/cpu_line.png'), (icon_rect.width, icon_rect.height))
     self.lcd.blit(icon_img, icon_rect)
 
-    text_surface = self.font_big.render('%s°' % self.metrics['temps']['cpu'], True, Sideshow.WHITE)
+    text_surface = self.font_big.render('%s°' % self.metrics['temps']['cpu'], True, Sideshow.OFF_WHITE)
     rect = text_surface.get_rect(center=(icon_rect.center[0], Sideshow.MARGIN + 55))
     self.lcd.blit(text_surface, rect)
 
-    icon_rect = pygame.Rect(320 - Sideshow.MARGIN - 100, Sideshow.MARGIN - 5, 100, 100)
+    icon_rect = pygame.Rect(Sideshow.SCREEN_WIDTH - Sideshow.MARGIN - LARGE_SQUARE_ICON_SIZE, Sideshow.MARGIN - 5, LARGE_SQUARE_ICON_SIZE, LARGE_SQUARE_ICON_SIZE)
     icon_img = pygame.transform.scale(pygame.image.load('resources/gpu_line.png'), (icon_rect.width, icon_rect.height))
     self.lcd.blit(icon_img, icon_rect)
 
-    text_surface = self.font_big.render('%s°' % self.metrics['temps']['gpu'], True, Sideshow.WHITE)
-    rect = text_surface.get_rect(center=(icon_rect.center[0], 240 - Sideshow.MARGIN - 40))
+    text_surface = self.font_big.render('%s°' % self.metrics['temps']['gpu'], True, Sideshow.OFF_WHITE)
+    rect = text_surface.get_rect(center=(icon_rect.center[0], Sideshow.SCREEN_HEIGHT - Sideshow.MARGIN - 40))
     self.lcd.blit(text_surface, rect)
 
     pygame.display.update()
@@ -113,7 +116,7 @@ class Sideshow():
     self.font_big = pygame.font.Font('resources/simplifica.ttf', 120)
 
     pygame.mouse.set_visible(False)
-    self.lcd = pygame.display.set_mode((320, 240))
+    self.lcd = pygame.display.set_mode((Sideshow.SCREEN_WIDTH, Sideshow.SCREEN_HEIGHT))
     self.lcd.fill((0,0,0))
     pygame.display.update()
 
